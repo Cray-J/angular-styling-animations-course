ab6d5b09f91804d690c328d914e6579452906d1b
Kristoffer Jensen <kje@computas.com>
2018-04-15 21:55:54 +0200

    Styling project list with animate and some keyframes

--- src/app/projects/animations.ts
+++ src/app/projects/animations.ts
@@ -1,4 +1,4 @@
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 export const markedTrigger = trigger('markedState', [
   state('default', style({
@@ -27,5 +27,41 @@ export const markedTrigger = trigger('markedState', [
       padding: '20px'
     }),
     animate('300ms ease-out')
+  ])
+]);
+
+export const itemStateTrigger = trigger('itemState', [
+  transition(':enter', [
+    // style({
+    //   opacity: 0,
+    //   transform: 'translateX(-100%)'
+    // }),
+    animate('500ms ease-out', keyframes([
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+        offset: 0
+      }),
+      style({
+        opacity: 1,
+        transform: 'translatex(15%)',
+        offset: 0.4
+      }),
+      style({
+        opacity: 1,
+        transform: 'translateX(0)',
+        offset: 1
+      })
+    ]))
+    // animate('500ms ease-out', style({
+    //   opacity: 1,
+    //   transform: 'translateX(0)'
+    // }))
+  ]),
+  transition(':leave', [
+    animate('500ms ease-out', style({
+      opacity: 0,
+      transform: 'translateX(100%)'
+    }))
   ])
 ]);
